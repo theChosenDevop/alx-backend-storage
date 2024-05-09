748d32a8557a8ac7dd34a1cc0ee771d6813e8f1f
theChosenDevop <tobbey.adesanya@email.com>
2024-05-09 21:33:58 +0000

    update file

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,4 @@
 -- SQL script create a table users
-CREATE DATABASE IF NOT EXISTS holberton;
-USE holberton;
 CREATE TABLE IF NOT EXISTS users (
 	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email varchar(255) NOT NULL UNIQUE,
