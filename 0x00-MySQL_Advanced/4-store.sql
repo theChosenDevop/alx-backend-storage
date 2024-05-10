2ac35cf22f5cc3d0a71db9557b3b83c1c355cfe7
theChosenDevop <tobbey.adesanya@email.com>
2024-05-10 04:49:55 +0000

    CREATE MYSQL TRIGGERS

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,12 @@
+-- SQL Script creates trigger that decreases the quantity of an item
+-- after adding a new order
+DELIMITER //
+CREATE TRIGGER reduceQty
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+	UPDATE items
+	SET quantity = quantity - NEW.number
+	WHERE name = NEW.item_name;
+END//
+DELIMITER;
