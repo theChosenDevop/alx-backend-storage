bf40fa6aa46586f3b7114606d258407712bc3f7c
theChosenDevop <tobbey.adesanya@gmail.com>
2024-05-16 04:03:36 +0000

    create redis client

--- 0x02-redis_basic/exerciseCopy.py
+++ 0x02-redis_basic/exerciseCopy.py
@@ -19,8 +19,8 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable] =
-            None) -> Union[str, bytes, int, float, None]:
+    def get(self, key: str, fn: Optional[Callable] = None)
+    -> Union[str, bytes, int, float, None]:
         """Returns the value"""
         value = self._redis.get(key)
         if value is None:
